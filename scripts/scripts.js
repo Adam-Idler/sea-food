43eeb193899c532a0a9796e40b3a14db344e7e65
Pavel Rakov <pasha_rakov@bk.ru>
2021-09-16 22:22:17 +0700

    Доделал немного функционала

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,6 @@
             responsive = []
         }) {
             if (!document.querySelector(wrap)) {
-                console.log(1);
                 return;
             }
             // элементы:
@@ -38,7 +37,7 @@
             this.addStyle();
             this.controlSlider();
             if (this.responsive) {
-            this.responseInit();
+                this.responseInit();
             }
         }
         
@@ -46,7 +45,7 @@
             this.main.classList.add('slider-carousel');
             this.wrap.classList.add('slider-carousel_wrap');
             for (const item of this.slides) {
-            item.classList.add('slider-carousel_item');
+                item.classList.add('slider-carousel_item');
             }
         }
         
@@ -54,24 +53,24 @@
             let style = document.getElementById('carousel-style');
             
             if (!style) {
-            style = document.createElement('style');
-            style.id = 'carousel-style';
+                style = document.createElement('style');
+                style.id = 'carousel-style';
             }
             style.textContent = `
-            .slider-carousel {
-                overflow: hidden;
-                position: relative;
-            }
-            .slider-carousel_wrap {
-                display: flex;
-                transition: transform 0.5s;
-                will-change: transform;
-            }
-            .slider-carousel_item {
-                display: flex;
-                flex-direction: column;
-                max-width: 100%;
-            }
+                .slider-carousel {
+                    overflow: hidden;
+                    position: relative;
+                }
+                .slider-carousel_wrap {
+                    display: flex;
+                    transition: transform 0.5s;
+                    will-change: transform;
+                }
+                .slider-carousel_item {
+                    display: flex;
+                    flex-direction: column;
+                    max-width: 100%;
+                }
             `;
             document.head.append(style);
         
@@ -82,21 +81,21 @@
         
         prevSlide() {
             if (this.infinity || this.position > 0) {
-            --this.position;
-            if (this.position < 0) {
-                this.position = this.maxPosition;
-            }
-            this.wrap.style.transform = `translateX(-${this.position * this.slideWidth}%)`;
+                --this.position;
+                if (this.position < 0) {
+                    this.position = this.maxPosition;
+                }
+                this.wrap.style.transform = `translateX(-${this.position * this.slideWidth}%)`;
             }
         }
         
         nextSlide() {
             if (this.infinity || this.position < this.maxPosition) {
-            ++this.position;
-            if (this.position > this.maxPosition) {
-                this.position = 0;
-            }
-            this.wrap.style.transform = `translateX(-${this.position * this.slideWidth}%)`;
+                ++this.position;
+                if (this.position > this.maxPosition) {
+                    this.position = 0;
+                }
+                this.wrap.style.transform = `translateX(-${this.position * this.slideWidth}%)`;
             }
         }
         
@@ -111,9 +110,9 @@
                 maxResponse = Math.max(...allResponse);
         
             const renderResponse = slToShow => {
-            this.slideWidth = Math.floor(100 / slToShow);
-            this.addStyle();
-            this.maxPosition = this.slides.length - slToShow;
+                this.slideWidth = Math.floor(100 / slToShow);
+                this.addStyle();
+                this.maxPosition = this.slides.length - slToShow;
             };
         
             const checkResponse = () => {
@@ -204,6 +203,23 @@
     });
     
     carousel3.init();
+
+    const carousel4 = new Carousel({
+        main: '.section_feedback-slider',
+        wrap: '.section_feedback-slider__wrapper',
+        prev: '.section_feedback-slider ~ .slider_news__arrow_left',
+        next: '.section_feedback-slider ~ .slider_news__arrow_right',
+        slidesToShow: 3,
+        infinity: true,
+        responsive: [
+            {
+            breakpoint: 576,
+            slidesToShow: 1
+            }
+        ]
+    });
+    
+    carousel4.init();
 })();
 
 // Кнопка "читать далее"
@@ -235,6 +251,10 @@
 
 // Табы
 (() => {
+    if (!document.querySelector('.section_tabs__tabs-list')) {
+        return;
+    }
+
     const tabHeader = document.querySelector('.section_tabs__tabs-list'),
           tab = tabHeader.querySelectorAll('.section_tabs__tabs-item'),
           tabContent = document.querySelectorAll('.section_tabs__tabs-item-value');
@@ -262,4 +282,167 @@
             });
         }
     });
+})();
+
+// Функционал каталога (кнопка "показать еще" и категории товаров)
+(() => {
+    class ShowMore {
+        constructor({
+            wrap,
+            button,
+            countShownItem = 3,
+            hiddenElementSelector = '.hidden-item'
+        }) {
+            if (!document.querySelector(wrap)) {
+                return;
+            }
+
+            this.wrap = document.querySelector(wrap);
+            this.button = document.querySelector(button);
+            this.countShownItem = countShownItem;
+            this.hiddenElementSelector = hiddenElementSelector;
+        }
+
+        showItem(item) {
+            if (!item) {
+                return
+            }
+            item.classList.remove(this.hiddenElementSelector.slice(1));
+        }
+
+        showItems() {
+            for (let i = 0; i < this.countShownItem; i++) {
+                this.showItem(this.wrap.querySelector(this.hiddenElementSelector));
+            }
+
+            if (!this.wrap.querySelector(this.hiddenElementSelector)) {
+                this.button.parentNode.classList.toggle('hidden-item');
+                this.button.removeEventListener('click', this.showItems.bind(this));
+            }
+        }
+
+        init() {
+            if (!this.wrap) {
+                return;
+            }
+            this.showItems();
+            this.button.addEventListener('click', this.showItems.bind(this));
+        }
+    }
+
+    class Categorys {
+        constructor({
+            section,
+            catalog,
+            catalogNav,
+            catalogNavItems,
+            catalogItems,
+            activeClassName
+        }) {
+            if (!document.querySelector(section)) {
+                return;
+            }
+
+            this.section = document.querySelector(section);
+            this.catalog = this.section.querySelector(catalog);
+            this.catalogNav = this.section.querySelector(catalogNav);
+            this.catalogNavItems = this.section.querySelectorAll(catalogNavItems)
+            this.catalogItems = this.section.querySelectorAll(catalogItems);
+            this.activeClassName = activeClassName;
+        }
+
+        removeChildren(parent) {
+            while (parent.firstChild) {
+                parent.removeChild(parent.firstChild);
+            }
+        }
+
+        updateChildren(parent, children) {
+            this.removeChildren(parent);
+            children.forEach(child => {
+                parent.appendChild(child);
+                child.classList.add('hidden-item');
+            })
+
+            catalogButton.showItems();
+        }
+
+        filter(e) {
+            
+        }
+
+        init() {
+            if (!this.catalogNav) {
+                return
+            }
+            this.catalogNav.addEventListener('click', (e) => {
+                let target = e.target;
+
+                if (!target || target.classList.contains(this.activeClassName)) {
+                    return;
+                }
+
+                e.preventDefault();
+                let filterValue = target.getAttribute('data-filter'),
+                    previousBtnActive = this.catalogNav.querySelector(`.${this.catalogNavItems[0].classList[0]}.${this.activeClassName}`),
+                    hiddenElements = this.section.querySelectorAll('.hidden-item');
+
+                previousBtnActive.classList.remove(this.activeClassName);
+                target.classList.add(this.activeClassName);
+
+                if (filterValue === 'all') {
+                    this.updateChildren(this.catalog, this.catalogItems);
+                    return;
+                }
+
+                let filteredItems = [];
+                filteredItems = [...this.catalogItems].filter(item => item.getAttribute('data-category').includes(filterValue));
+                if (hiddenElements === 0) {
+                    this.section.querySelector('.catalog-btn-show-more').parentNode.classList.add('hidden-item');
+                } else {
+                    this.section.querySelector('.catalog-btn-show-more').parentNode.classList.remove('hidden-item');
+                }
+
+                this.updateChildren(this.catalog, filteredItems);
+            });
+        }
+    }
+
+    const catalogButton = new ShowMore({
+        wrap: '.stocks__catalog',
+        button: '.catalog-btn-show-more',
+        countShownItem: 6
+    });
+
+    catalogButton.init();
+
+    const newsButton = new ShowMore({
+        wrap: '.section_news-page__wrapper',
+        button: '.section_news-page .catalog-btn-show-more',
+    })
+
+    newsButton.init();
+
+    const stocksCatalog = new Categorys({
+        section: '.section_stocks',
+        catalog: '.stocks__catalog',
+        catalogNav: '.stocks__categories',
+        catalogNavItems: '.stocks__categories-item',
+        catalogItems: '.stocks__catalog-item',
+        activeClassName: 'stocks__categories-item_active'
+    });
+
+    stocksCatalog.init();
+
+    const mainCatalog = new Categorys({
+        section: '.section_catalog',
+        catalog: '.stocks__catalog',
+        catalogNav: '.section_catalog__aside',
+        catalogNavItems: '.section_catalog__aside-item',
+        catalogItems: '.stocks__catalog-item',
+        activeClassName: 'section_catalog__aside-item_active'
+    });
+
+    mainCatalog.init();
+
 })();
